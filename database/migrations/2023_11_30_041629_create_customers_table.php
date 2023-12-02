1fb93469307344c0d141aad66be9c5c7dd2c6359
joao-silva09 <joao.e.silva.03@gmail.com>
2023-12-02 18:48:45 -0300

    Run Customers migrations.

--- database/migrations/2023_11_30_041629_create_customers_table.php
+++ database/migrations/2023_11_30_041629_create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email');
             $table->date('entry_date');
             $table->unsignedBigInteger('company_id');
             $table->foreign('company_id')->references('id')->on('companies');
