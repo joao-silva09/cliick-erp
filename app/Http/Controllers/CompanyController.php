d5e12e7a4891116f4bc0fb9f39530db1b15408c6
joao-silva09 <joao.e.silva.03@gmail.com>
2023-12-02 19:24:52 -0300

    Add Customer and Teams in Company request.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -12,7 +12,7 @@ class CompanyController extends Controller
      */
     public function index(Request $request)
     {
-        $companies = Company::all();
+        $companies = Company::with('customers')->with('teams')->paginate(2);
         return $companies;
     }
 
