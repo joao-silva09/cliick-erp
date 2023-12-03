252b2e6243a79782ec2ca7438c364abc00320351
joao-silva09 <joao.e.silva.03@gmail.com>
2023-12-03 00:04:06 -0300

    Add relationships in Customer model.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -15,5 +16,10 @@ class Customer extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 }
