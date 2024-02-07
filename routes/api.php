f74b313731fe88ac78ea7992cf8b6c587a37f3dd
joao-silva09 <joao.e.silva.03@gmail.com>
2024-02-07 15:21:04 -0300

    CORS Policy.

--- routes/api.php
+++ routes/api.php
@@ -23,9 +23,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
