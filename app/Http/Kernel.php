ef9c5c45ec52f02efda305df8f65d3984da0e5bb
joao-silva09 <joao.e.silva.03@gmail.com>
2024-02-07 16:56:32 -0300

    CORS policy.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,4 +68,10 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        // ...
+        'cors' => \App\Http\Middleware\Cors::class,
+    ];
+    
 }
