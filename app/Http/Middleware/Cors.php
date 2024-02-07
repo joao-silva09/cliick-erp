5a087c01a3a9381a06329db14187493f52cc29c9
joao-silva09 <joao.e.silva.03@gmail.com>
2024-02-07 16:19:01 -0300

    CORS policy.

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Origin' => 'http://localhost:5173/',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
         ];
