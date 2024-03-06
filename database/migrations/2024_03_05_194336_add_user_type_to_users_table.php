f1b6530096fd9d9a6d539b61abad3723bce7f2f8
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-06 16:07:40 -0300

    Re-run migration add_user_type_to_users_table.

--- database/migrations/2024_03_05_194336_add_user_type_to_users_table.php
+++ database/migrations/2024_03_05_194336_add_user_type_to_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
 {
     Schema::table('users', function (Blueprint $table) {
         $table->string('user_type');
+        $table->string('phone');
     });
 }
 
@@ -23,7 +24,7 @@ return new class extends Migration
     public function down()
 {
     Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn('telefone');
+        $table->dropColumn('user_type');
     });
 }
 
