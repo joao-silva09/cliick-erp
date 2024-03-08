4e27f5f16b7dcd67a0643ead2bdeef35178685b7
joao-silva09 <joao.e.silva.03@gmail.com>
2024-03-08 16:27:20 -0300

    Run updated user migration.

--- database/migrations/2024_01_18_202401_create_users_table.php
+++ database/migrations/2024_01_18_202401_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
+            $table->string('user_type');
+            $table->string('phone');
+            $table->string('profile_photo')->nullable();
             $table->string('confirmation_token')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
