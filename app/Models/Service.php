3966f3b3e507f941f554c1e34185e7157828c569
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-08 23:18:11 -0300

    Created Service model.

---
+++ app/Models/Service.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'base_value',
+        'recurrence',
+        'active'
+    ];
+
+    public function contracts()
+    {
+        return $this->belongsToMany(Contract::class);
+    }
+}
