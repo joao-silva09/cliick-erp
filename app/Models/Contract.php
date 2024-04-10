f25fd47f851735b648d58b81cc21f4120d915b3d
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-09 23:53:26 -0300

    Created Contract model.

---
+++ app/Models/Contract.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'status',
+        'customer_id'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+    
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
+}
