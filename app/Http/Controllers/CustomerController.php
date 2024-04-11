49c3a4815c764e51324f3b76fefd828a53fa7224
joao-silva09 <joao.e.silva.03@gmail.com>
2024-04-11 16:58:56 -0300

    Updated customer store method to add customer_logo.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\CustomerCollection;
 use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CustomerController extends Controller
 {
@@ -25,7 +26,22 @@ class CustomerController extends Controller
      */
     public function store(CustomerRequest $request)
     {
+        $request->validate([
+            'customer_logo' => 'required|image|max:2048', // Limita o arquivo a 2MB e deve ser uma imagem
+        ]);
+
         $customer = Customer::create($request->all());
+
+        // Faz o upload da nova logo e retorna o caminho
+        $path = $request->file('customer_logo')->store('customer_logos', 'public');
+
+        // Atualiza o caminho da foto de perfil no banco de dados
+        $customer->customer_logo = $path;
+        $customer->save();
+  
+        // Gera a URL pública para a imagem
+        // $photoUrl = Storage::url($customer->logo);
+
         return new CustomerResource($customer);
     }
 
